47735b59934ae30cf0ac98e1d7132cf66de0b59a
Ask <34611138+FaNzu@users.noreply.github.com>
2024-03-22 17:47:21 +0100

    climber and defender movement finished

--- Players/climber.gd
+++ Players/climber.gd
@@ -3,10 +3,42 @@ extends Area2D
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
+signal hit
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
+	#uncomment to hide character on start
+	#hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("climber_move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("climber_move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("climber_move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("climber_move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false

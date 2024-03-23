cbd6da00ef9988365ca3408840e4098eb1263abf
Ask <34611138+FaNzu@users.noreply.github.com>
2024-03-23 13:56:10 +0100

    game splitscreen working
    splitscreen working, clamp need editing, and world working

--- MainMenu.gd
+++ MainMenu.gd
@@ -7,4 +7,4 @@ func _on_exit_button_pressed():
 	get_tree().quit()
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Levels/Castle.tscn")
+	get_tree().change_scene_to_file("res://game.tscn")

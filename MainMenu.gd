d410ac1d6818e45821d87fc6067bcf3890d14173
Ask <34611138+FaNzu@users.noreply.github.com>
2024-03-23 12:16:36 +0100

    adding pictures and game scene

---
+++ MainMenu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	pass
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/Castle.tscn")
